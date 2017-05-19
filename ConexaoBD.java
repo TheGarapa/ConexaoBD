a5036f051a0d7c29758657ee840d798408f862f2
Harley Sousa <harley@harley>
2017-05-19 19:10:59 -0300

    Conexão com banco de dados

---
+++ ConexaoBD.java
@@ -0,0 +1,33 @@
+package ProjetoUniao;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public class ConexaoBD {
+
+	public Statement stm;
+	public ResultSet rs;
+	private String driver = "org.postgresql.Driver";
+	private String url = "jdbc:postgresql://localhost:5432/Fran_Eventos"; 
+	private String usuario = "postgres";
+	private String senha = "harley";
+	public Connection con;
+
+	public void Conexao(){
+	  
+	    try {
+//		System.setProperty("jdbc.Drivers", driver);
+		con=DriverManager.getConnection(url, usuario, senha);
+		JOptionPane.showMessageDialog(null, "Conexão bem sucedida!");
+                
+} // chaves da linha try
+
+		catch (SQLException ex) {
+		JOptionPane.showMessageDialog(null, "Não foi possível se conectar ao banco de dados: "+ex);
+} // chaves da linha catch
+  
+    }    
+ 
+}   
